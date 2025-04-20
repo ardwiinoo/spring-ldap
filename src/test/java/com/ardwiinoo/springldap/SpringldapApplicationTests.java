bc3a48738a65177b28bf07de628ecda9a99b0df1
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2025-04-20 14:19:46 +0700

    chore: init

---
+++ src/test/java/com/ardwiinoo/springldap/SpringldapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardwiinoo.springldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringldapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
